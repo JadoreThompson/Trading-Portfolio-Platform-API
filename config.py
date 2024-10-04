5d9ef1e801da8a72feb3b5a5b05f779a6d33cc80
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2024-10-04 01:38:18 +0100

    alpaca setup and working

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
+import alpaca_trade_api as trade_api
 
 
 load_dotenv()
 
 # Alpaca
-ALPACA_BASE_URL: str = "https://api.alpaca.markets/v1/assets"
-ALPACA_API_KEY: str = os.getenv('ALPACA_SANDBOX_API_KEY')
-ALPACA_SECRET_KEY: str = os.getenv('ALPACA_SANDBOX_SECRET_KEY')
+ALPACA_BASE_URL = "https://paper-api.alpaca.markets"
+ALPACA_API_KEY = os.getenv('ALPACA_SANDBOX_API_KEY')
+ALPACA_SECRET_KEY = os.getenv('ALPACA_SANDBOX_SECRET_KEY')
+alpaca = trade_api.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, ALPACA_BASE_URL, api_version='v2')
